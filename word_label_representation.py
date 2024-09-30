50336e66372b8f1bfa51e7d89a2f4055ded49e56
nnminh322 <nnminh322@gmail.com>
2024-09-30 16:42:23 +0700

    update model.py

--- word_label_representation.py
+++ word_label_representation.py
@@ -22,23 +22,28 @@ class word_and_label_representation(nn.Module):
         self.label_embeddings = torch.nn.Embedding(self.num_labels, self.hidden_size)
         nn.init.xavier_uniform_(self.label_embeddings.weight)
 
-    def forward(self, sentence):
+        self.trigger_ffn = nn.Linear(self.hidden_size, 1)
+        self.type_ffn = nn.Linear(
+            self.hidden_size + self.hidden_size, 1
+        )  # 2 * Hidden_size -> 1
 
-        tokenizer = BertTokenizerFast.from_pretrained(self.model_name)
+    def forward(self, input_ids, attention_mask, token_type_ids):
 
-        encoding = tokenizer(
-            sentence,
-            is_split_into_words=True,
-            return_tensors="pt",
-            return_attention_mask=True,
-            return_token_type_ids=True,
-            padding=True,
-            Truncation=True,
-        )
+        # tokenizer = BertTokenizerFast.from_pretrained(self.model_name)
 
-        input_ids = encoding["input_ids"].to(self.device)
-        attention_mask = encoding["attention_mask"].to(self.device)
-        token_type_ids = encoding["token_type_tds"].to(self.device)
+        # encoding = tokenizer(
+        #     sentence,
+        #     is_split_into_words=True,
+        #     return_tensors="pt",
+        #     return_attention_mask=True,
+        #     return_token_type_ids=True,
+        #     padding=True,
+        #     Truncation=True,
+        # )
+
+        # input_ids = encoding["input_ids"].to(self.device)
+        # attention_mask = encoding["attention_mask"].to(self.device)
+        # token_type_ids = encoding["token_type_tds"].to(self.device)
 
         with torch.no_grad:
             outputs = self.bert(
@@ -47,33 +52,51 @@ class word_and_label_representation(nn.Module):
                 token_type_ids=token_type_ids,
             )
 
-        last_hidden_state = outputs.last_hidden_state
+            last_hidden_state = outputs.last_hidden_state
+            e_cls = last_hidden_state[:, 0, :]  # [Batch_size, hidden_size]
 
-        word_ids = encoding.word_ids(batch_index=0)
-        cls_embedding = last_hidden_state[0, 0, :]
-        E = [cls_embedding]
+        # Trigger Identification
+        p_wi = torch.sigmoid(self.trigger_ffn(last_hidden_state)).squeeze(-1)
 
-        current_word = None
-        current_embedding = []
+        # Type Prediction
 
-        for idx, word_id in enumerate(word_ids):
-            if word_id is None:
-                continue
-            if word_id != current_word:
-                if current_embedding:
-                    averaged_embedding = torch.stack(current_embedding, dim=0).mean(
-                        dim=0
-                    )
-                    E.append(averaged_embedding)
-                current_word = word_id
-                current_embedding = []
-            current_embedding.append(last_hidden_state[0, idx, :])
+        label_embeddings = self.label_embeddings.weight  # [Num_label, hidden_size]
+        label_embeddings = label_embeddings.unsqueeze(0).repeat(input_ids.size[0], 1, 1)  # [Num_label, hidden_size] -> [Batch_size, Num_label, Hidden_size]
+        e_cls = e_cls.unsqueeze(1).repeat(1, self.num_labels, 1) # [Batch_size, hidden_size] -> [Batch_size, Num_label, hidden_size]
+        concat = torch.cat([label_embeddings, e_cls], dim=-1) # Concat in last size dimention
 
-        if current_embedding:
-            averaged_embedding = torch.stack(current_embedding, dim=0).mean(dim=0)
-            E.append(averaged_embedding)
+        p_ti = torch.sigmoid(self.type_ffn(concat)).squeeze(-1)
 
-        E = torch.stack(E, dim=0)
-        T = self.label_embeddings.weight
+        return p_wi, p_ti, last_hidden_state, e_cls
+    
+    def get_label_embedding(self):
+        return self.label_embeddings.weight
 
-        return E, T
+        # word_ids = encoding.word_ids(batch_index=0)
+        # cls_embedding = last_hidden_state[0, 0, :]
+        # E = [cls_embedding]
+
+        # current_word = None
+        # current_embedding = []
+
+        # for idx, word_id in enumerate(word_ids):
+        #     if word_id is None:
+        #         continue
+        #     if word_id != current_word:
+        #         if current_embedding:
+        #             averaged_embedding = torch.stack(current_embedding, dim=0).mean(
+        #                 dim=0
+        #             )
+        #             E.append(averaged_embedding)
+        #         current_word = word_id
+        #         current_embedding = []
+        #     current_embedding.append(last_hidden_state[0, idx, :])
+
+        # if current_embedding:
+        #     averaged_embedding = torch.stack(current_embedding, dim=0).mean(dim=0)
+        #     E.append(averaged_embedding)
+
+        # E = torch.stack(E, dim=0)
+        # T = self.label_embeddings.weight
+
+        # return E, T
