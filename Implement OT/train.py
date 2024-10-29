0f9c12061c386c4606674d685cdddc1b2d72c09a
nnminh322 <nnminh322@gmail.com>
2024-10-29 13:25:18 +0700

    test ot

--- Implement OT/train.py
+++ Implement OT/train.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from transformers import BertTokenizerFast
 from data import Event_Detection_Dataset
 from model import EDmodel
-from ot_ultis import compute_optimal_transport
+from ot_utils import compute_optimal_transport
 import torch.nn.functional as F
 import torch.optim as optim
 
