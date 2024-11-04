1bd612e7d5ebaaaa000a9df86c0790d26502664d
nnminh322 <nnminh322@gmail.com>
2024-11-04 22:49:13 +0700

    Update MAVEN_all_fwUCL+TCL.sh

--- HANEt origin/MAVEN_all_fwUCL+TCL.sh
+++ HANEt origin/MAVEN_all_fwUCL+TCL.sh
@@ -27,7 +27,7 @@ do
                             --dweight_loss \
                             --rep-aug mean \
                             --distill mul \
-                            --epoch 15 \
+                            --epoch 30 \
                             --class-num $m \
                             --single-label \
                             --cl-aug $k \
